bfbdea401d158e1456b10a351ce8727d54faa7f8
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-11-17 19:05:31 -0300

    Setup inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterializeModule } from 'ng2-materialize';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule.forRoot(),
+    ColorPickerModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
