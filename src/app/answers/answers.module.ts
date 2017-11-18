08ab560264779c8d756980121448d100158c112a
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-11-18 18:03:46 -0300

    Generate answers graphs

--- src/app/answers/answers.module.ts
+++ src/app/answers/answers.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnswerListComponent } from './answer-list/answer-list.component';
 import { AnswerNewComponent } from './answer-new/answer-new.component';
+import { GraphNumberComponent } from './shared/graph-number/graph-number.component';
+import { GraphTextComponent } from './shared/graph-text/graph-text.component';
+import { GraphBooleanComponent } from './shared/graph-boolean/graph-boolean.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AnswerListComponent, AnswerNewComponent]
+  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent, GraphTextComponent, GraphBooleanComponent]
 })
 export class AnswersModule { }
